c0fff39f610a53afe557a0cf1f804493d38822c6
SylveonDeko <crimsonslayer76@gmail.com>
2023-09-05 09:30:52 -0400

    fix premium ratelimit

--- BoostBotV2/Common/Attributes/TextCommands/Ratelimit.cs
+++ BoostBotV2/Common/Attributes/TextCommands/Ratelimit.cs
@@ -11,7 +11,7 @@ public class RateLimitAttribute : PreconditionAttribute
     private static readonly Dictionary<ulong, DateTime> UserTimeouts = new();
 
     // Set a limit on the number of seconds between messages
-    private TimeSpan _timeout;
+    private readonly TimeSpan _timeout;
 
     public RateLimitAttribute(int seconds)
     {
@@ -21,21 +21,27 @@ public class RateLimitAttribute : PreconditionAttribute
     public override Task<PreconditionResult> CheckPermissionsAsync(ICommandContext context, CommandInfo command, IServiceProvider services)
     {
         var user = context.User as IGuildUser;
+
+        // Use a local variable for timeout
+        var currentTimeout = _timeout;
+
         if (user.RoleIds.Contains<ulong>(1136525445693706370))
         {
-            _timeout = TimeSpan.FromSeconds(_timeout.Seconds / 2);
+            currentTimeout = TimeSpan.FromSeconds(_timeout.Seconds / 2);
         }
+
         var creds = services.GetService<Credentials>();
         if (creds.Owners.Contains(context.User.Id))
         {
             return Task.FromResult(PreconditionResult.FromSuccess());
         }
+
         if (UserTimeouts.TryGetValue(context.User.Id, out var timeout))
         {
             var timeSinceLastMessage = DateTime.UtcNow - timeout;
-            if (timeSinceLastMessage < _timeout)
+            if (timeSinceLastMessage < currentTimeout)
             {
-                return Task.FromResult(PreconditionResult.FromError($"Please wait {_timeout.TotalSeconds} seconds between commands."));
+                return Task.FromResult(PreconditionResult.FromError($"Please wait {currentTimeout.TotalSeconds} seconds between commands."));
             }
         }
 
@@ -43,4 +49,5 @@ public class RateLimitAttribute : PreconditionAttribute
 
         return Task.FromResult(PreconditionResult.FromSuccess());
     }
+
 }
